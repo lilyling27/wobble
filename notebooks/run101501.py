24168434b1d35a00e7b8651fca75e6cf860f4969
lily <lily.zhao@yale.edu>
2020-09-15 14:20:01 -0400

    Test New Pipeline RVs

--- notebooks/run101501.py
+++ notebooks/run101501.py
@@ -10,7 +10,9 @@ import tensorflow as tf
 tf.__version__
 
 date = '200907'
-file_num = 1 # initial test
+file_num = 1 # a very very initial test
+# Keeping the file_num since the first test was very way bad.
+# Running again with rv_opt_steps added and pipeline RVs changed
 
 K_t = 2
 
@@ -34,14 +36,14 @@ results = wobble.Results(data=data)
 for r in range(len(data.orders)):
     print('starting order {0} of {1}'.format(r+1, len(data.orders)))
     model = wobble.Model(data, results, r)
-    model.add_star('star', regularization_par_file=reg_star_file)
+    model.add_star('star', regularization_par_file=reg_star_file, rv_opt_steps=20)
     #model.add_star('star', regularization_par_file=reg_star_file,
     #               learning_rate_template=lnrs['learning_rate_template'][r], rv_steps=20)
     model.add_telluric('tellurics', regularization_par_file=reg_t_file, variable_bases=K_t)
     #model.add_telluric('tellurics', regularization_par_file=reg_t_file, variable_bases=K_t,
     #                   learning_rate_template=lnrt['learning_rate_template'][r])
     #wobble.optimize_order(model,save_history=True,min_dnll=1e-4,niter=1500,return_best_iter=True)
-    wobble.optimize_order(model,save_history=True,basename=hist_base,niter=1500)
+    wobble.optimize_order(model,save_history=True,basename=hist_base,niter=250)
 results.combine_orders('star')
 results.write_rvs('star', f'./Results/{date}_{star_name}_rvs{file_num}.txt')
 results.write(f'./Results/{date}_{star_name}_results{file_num}.hdf5')
